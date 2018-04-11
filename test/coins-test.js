525cb7a4076fd77a4df1b31dde195aab47f2e84a
Jonathan Gonzalez <rojikkushisutemu@gmail.com>
2018-04-11 22:50:08 +0000

    tests: Coins / Coinview added

--- test/coins-test.js
+++ test/coins-test.js
@@ -11,8 +11,10 @@ const Outpoint = require('../lib/primitives/outpoint');
 const CoinView = require('../lib/coins/coinview');
 const CoinEntry = require('../lib/coins/coinentry');
 const common = require('./util/common');
+const Coin = require('../lib/primitives/coin');
 
 const tx1 = common.readTX('tx1');
+const coin1 = common.readFile('coin1.raw');
 
 function reserialize(coin) {
   const raw = coin.toRaw();
@@ -98,5 +100,34 @@ describe('Coins', function() {
     assert.strictEqual(coins.outputs.size, 1);
     assert.strictEqual(coins.get(0), null);
     deepCoinsEqual(coins.get(1), reserialize(coins.get(1)));
+  });
+
+   it('should instantiate from tx', () => {
+    const [tx] = tx1.getTX();
+    const coin = Coin.fromTX(tx, 0, 0);
+
+    assert.strictEqual(coin.getAddress().toString(),
+      '3KUER9kZ693d5FQgvmr5qNDKnSpP9nXv9v');
+    assert.strictEqual(coin.value, 5000000);
+    assert.strictEqual(coin.getType(), 'multisig');
+    assert.strictEqual(coin.version, 1);
+    assert.strictEqual(coin.height, 0);
+    assert.strictEqual(coin.coinbase, false);
+    assert.strictEqual(coin.txid(),
+      'ff80fe4937e2de16411c3a2bc534d661dc8b4f8aad75e6fbc4b1ec6060d9ef1c');
+    assert.strictEqual(coin.index, 0);
+  });
+
+  it('should instantiate from raw', () => {
+    const coin = Coin.fromRaw(coin1);
+
+    assert.strictEqual(coin.getAddress().toString(),
+      '3KUER9kZ693d5FQgvmr5qNDKnSpP9nXv9v');
+    assert.strictEqual(coin.value, 5000000);
+    assert.strictEqual(coin.getType(), 'multisig');
+    assert.strictEqual(coin.version, 1);
+    assert.strictEqual(coin.height, 0);
+    assert.strictEqual(coin.coinbase, false);
+    assert.strictEqual(coin.index, 0);
   });
 });
